86d895b6a8df3076535e3f8325ab78a2ae57f8a6
Deadshot Ryker <ryker@zerolend.xyz>
2024-03-20 06:03:42 +0530

    fix for blast network

--- src/mapping/address-provider-registry/v3.ts
+++ src/mapping/address-provider-registry/v3.ts
@@ -1,9 +1,10 @@
-import { Address } from '@graphprotocol/graph-ts';
+import { Address, BigInt } from '@graphprotocol/graph-ts';
 import { Pool } from '../../../generated/schema';
 import { PoolAddressesProvider } from '../../../generated/templates';
 import {
   AddressesProviderRegistered,
   AddressesProviderUnregistered,
+  OwnershipTransferred,
 } from '../../../generated/PoolAddressesProviderRegistry/PoolAddressesProviderRegistry';
 import { getProtocol } from '../../helpers/v3/initializers';
 
@@ -30,5 +31,22 @@ export function handleAddressesProviderUnregistered(event: AddressesProviderUnre
     pool.active = false;
     pool.lastUpdateTimestamp = event.block.timestamp.toI32();
     pool.save();
+  }
+}
+
+export function handleOwnershipTransferred(event: OwnershipTransferred): void {
+  let protocol = getProtocol();
+  let address = '0xb0811a1fc9fb9972ee683ba04c32cb828bcf587b'
+
+  if (Pool.load(address) == null) {
+    let pool = new Pool(address);
+    pool.protocol = protocol.id;
+    pool.addressProviderId = BigInt.fromI32(30);
+    pool.active = true;
+    pool.paused = false;
+    pool.lastUpdateTimestamp = event.block.timestamp.toI32();
+    pool.save();
+
+    PoolAddressesProvider.create(Address.fromString(address));
   }
 }
